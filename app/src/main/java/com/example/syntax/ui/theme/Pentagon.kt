ed3e9264c77592d800b4a976eec815d3a9a24a2f
Uchenna Ibeziako <ucibeziako@gmail.com>
2026-09-22 18:32:26 -0600

    cOMPLETED

--- app/src/main/java/com/example/syntax/ui/theme/Pentagon.kt
+++ app/src/main/java/com/example/syntax/ui/theme/Pentagon.kt
@@ -3,6 +3,6 @@ package com.example.syntax.ui.theme
 import Shape
 
 class Pentagon: Shape() {
-    override val x: Int = 3
-    override val y: Int = 3
+    override val x: Int = 5
+    override val y: Int = 5
 }
